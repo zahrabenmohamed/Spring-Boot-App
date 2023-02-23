a69c371fa9ddb866efe8b817c8cd42d55751aa41
zahrabenmohamed <benmedzahra20@gmail.com>
2023-02-23 14:46:47 +0100

    init commit-MVC-Pattern

--- src/main/java/com/zahra/appLearning/controller/HomeController.java
+++ src/main/java/com/zahra/appLearning/controller/HomeController.java
@@ -2,14 +2,14 @@ package com.zahra.appLearning.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value={" ","/","/home"})
-    public String DisplayHomePage(){
+    public String DisplayHomePage(Model model){
         return "home.html" ;
     }
 
