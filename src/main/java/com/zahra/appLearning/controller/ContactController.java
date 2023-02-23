72031d3fdd1bf0a9ffc0c704cd553933778d6f1a
zahrabenmohamed <benmedzahra20@gmail.com>
2023-02-23 22:39:20 +0100

    last commit in the session

--- src/main/java/com/zahra/appLearning/controller/ContactController.java
+++ src/main/java/com/zahra/appLearning/controller/ContactController.java
@@ -1,9 +1,11 @@
 package com.zahra.appLearning.controller;
 
 import com.zahra.appLearning.model.Contact;
+import com.zahra.appLearning.service.ContactService;
 import lombok.extern.java.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +16,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ContactController {
 
-    private  static Logger log=LoggerFactory.getLogger(ContactController.class);
+    @Autowired
+    private ContactService contactService;
+
 
     @RequestMapping(value={"/contact"})
     public String DisplayContactPage(){
@@ -23,6 +27,8 @@ public class ContactController {
 
     @RequestMapping(value={"/saveMsg"},method = RequestMethod.POST)
     public ModelAndView saveMessage(@RequestParam Contact contact){
+
+        contactService.saveMessageDetail(contact);
         
         return  new ModelAndView("redirect:/contact");
 
--- src/main/java/com/zahra/appLearning/service/ContactService.java
+++ src/main/java/com/zahra/appLearning/service/ContactService.java
@@ -1,7 +1,16 @@
 package com.zahra.appLearning.service;
 
+import com.zahra.appLearning.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ContactService {
+
+    public Boolean saveMessageDetail(Contact contact){
+        boolean isSaved=true;
+        log.info(contact.toString());
+        return isSaved;
+    }
 }
