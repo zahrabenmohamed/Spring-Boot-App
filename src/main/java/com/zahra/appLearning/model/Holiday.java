c19ee4d4bee5e741b6252c91a32675bf8e3f5611
zahrabenmohamed <benmedzahra20@gmail.com>
2023-02-23 22:16:49 +0100

    using one lombook feature

--- src/main/java/com/zahra/appLearning/model/Holiday.java
+++ src/main/java/com/zahra/appLearning/model/Holiday.java
@@ -8,13 +8,6 @@ public class Holiday {
     private final String day;
     private final String reason;
     private final Type type;
-
-    public Holiday(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
-
     public enum Type{
         FESTIVAL,FEDERAL
     }
