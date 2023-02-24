ac4743b4601321360a70bea6f0423f8a3ca21b22
zahrabenmohamed <benmedzahra20@gmail.com>
2023-02-24 14:37:12 +0100

    edit config file

--- src/main/java/com/zahra/appLearning/config/ProjectSecurityConfig.java
+++ src/main/java/com/zahra/appLearning/config/ProjectSecurityConfig.java
@@ -13,10 +13,14 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
 
-        httpSecurity.authorizeHttpRequests().anyRequest()
-                .denyAll().and()
-                .formLogin().and()
-                .httpBasic();
+        httpSecurity.authorizeHttpRequests().mvcMatchers("/home/**").permitAll()
+                .mvcMatchers("/courses").denyAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/about").permitAll()
+                .and().formLogin().and().httpBasic();
+
+
+
 
     }
 
