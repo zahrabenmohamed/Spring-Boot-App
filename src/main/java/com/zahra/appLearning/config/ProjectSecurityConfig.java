d337b746fc6746801e6b650daba7d9251a3cb59a
zahrabenmohamed <benmedzahra20@gmail.com>
2023-02-24 15:30:23 +0100

    edit config file

--- src/main/java/com/zahra/appLearning/config/ProjectSecurityConfig.java
+++ src/main/java/com/zahra/appLearning/config/ProjectSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.zahra.appLearning.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -13,16 +16,27 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
 
-        httpSecurity.authorizeHttpRequests().mvcMatchers("/home/**").permitAll()
-                .mvcMatchers("/courses").denyAll()
-                .mvcMatchers("/contact").permitAll()
-                .mvcMatchers("/about").permitAll()
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests().mvcMatchers("/home/**").permitAll()
+                .mvcMatchers("/courses").authenticated()
+                .mvcMatchers("/contact").authenticated() //let only authenticated user to contact the company
+                .mvcMatchers("/about").authenticated()
                 .and().formLogin().and().httpBasic();
 
 
 
 
     }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("{noop}password1").roles("USER")
+                .and()
+                .withUser("user2").password("{noop}password2").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password3").roles("ADMIN");
+    }
+
 
 
 
